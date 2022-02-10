db140b901d697f4718545e8b79b9dad48b1a5a2c
Alexander Chudov <usatu.robotics@gmail.com>
2022-02-10 18:19:33 +0500

    added synchronization field

---
+++ app/src/main/java/com/rickrip/javaone/concurrency/CommonResource.java
@@ -0,0 +1,2 @@
+package com.rickrip.javaone.concurrency;public class CommonResource {
+}
---
+++ app/src/main/java/com/rickrip/javaone/concurrency/ThreadsWithCommonResource.java
@@ -0,0 +1,5 @@
+package com.rickrip.javaone.concurrency;
+
+public class CommonResource {
+    float num = 0.0001f;
+}
